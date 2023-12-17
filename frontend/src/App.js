4de6cb8b2e9397770e8501b5f8cf493367be3bc1
Evelyn9999 <sapphiry0404@gmail.com>
2023-12-17 22:07:28 +0200

    app.js modified with itemlist added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Total from './Total';
+import AddItem from './AddItem';
+import ItemList from './ItemList';
+
+import React, { useState, useEffect } from 'react';
 
 function App() {
+	const [items, setItems] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const [totalItemCount, setTotalItemCount] = useState(6);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app-background'>
+			<div className='main-container'>
+			<h1>Shopping List:</h1>
+			<ItemList
+				// Prop: Pass the current state of items to the ItemList component,same for other function
+				items={items}
+				/>
+			<Total totalItemCount={totalItemCount} />
+			</div>
+		</div>
   );
 }
 
